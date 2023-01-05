e1e45df3c7a89f60858831fe877a8ae9d5d56de6
JHVanGilder <42916735+JHVanGilder@users.noreply.github.com>
2023-01-05 14:09:39 -0600

    Add files via upload

---
+++ CS540/hw7/student_code.py
@@ -0,0 +1,123 @@
+# python imports
+import os
+from tqdm import tqdm
+
+# torch imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# helper functions for computer vision
+import torchvision
+import torchvision.transforms as transforms
+
+
+class LeNet(nn.Module):
+    def __init__(self, input_shape=(32, 32), num_classes=100):
+        super(LeNet, self).__init__()
+        # certain definitions
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1)
+        self.relu1 = nn.ReLU()
+        self.maxPooling1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1)
+        self.relu2 = nn.ReLU()
+        self.maxPooling2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.flattenLayer = nn.Flatten()
+
+        self.lin1 = nn.Linear(400, 256)
+        self.lin2 = nn.Linear(256, 128)
+        self.lin3 = nn.Linear(128, 100)
+
+    def forward(self, x):
+        shape_dict = {}
+        # certain operations
+        x = nn.functional.relu(self.conv1(x))
+        x = self.maxPooling1(x)
+        shape_dict[1] = x.size()
+        
+        x = nn.functional.relu(self.conv2(x))
+        x = self.maxPooling2(x)
+        shape_dict[2] = x.size()
+
+        x = self.flattenLayer(x)
+        shape_dict[3] = x.size()
+
+        x = nn.functional.relu(self.lin1(x))
+        shape_dict[4] = x.size()
+
+        x = nn.functional.relu(self.lin2(x))
+        shape_dict[5] = x.size()
+        
+        x = self.lin3(x)
+        shape_dict[6] = x.size()
+
+        out = x
+        return out, shape_dict
+
+
+def count_model_params():
+    '''
+    return the number of trainable parameters of LeNet.
+    '''
+    model = LeNet()
+    model_params = 0.0
+    for parameter in list(model.parameters()):
+        par = 1
+        for s in list(parameter.size()):
+            par = par*s
+        model_params += par
+        
+    return model_params/1e6
+
+
+def train_model(model, train_loader, optimizer, criterion, epoch):
+    """
+    model (torch.nn.module): The model created to train
+    train_loader (pytorch data loader): Training data loader
+    optimizer (optimizer.*): A instance of some sort of optimizer, usually SGD
+    criterion (nn.CrossEntropyLoss) : Loss function used to train the network
+    epoch (int): Current epoch number
+    """
+    model.train()
+    train_loss = 0.0
+    for input, target in tqdm(train_loader, total=len(train_loader)):
+        ###################################
+        # fill in the standard training loop of forward pass,
+        # backward pass, loss computation and optimizer step
+        ###################################
+
+        # 1) zero the parameter gradients
+        optimizer.zero_grad()
+        # 2) forward + backward + optimize
+        output, __ = model(input)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+
+        # Update the train_loss variable
+        # .item() detaches the node from the computational graph
+        # Uncomment the below line after you fill block 1 and 2
+        train_loss += loss.item()
+
+    train_loss /= len(train_loader)
+    print('[Training set] Epoch: {:d}, Average loss: {:.4f}'.format(epoch+1, train_loss))
+
+    return train_loss
+
+
+def test_model(model, test_loader, epoch):
+    model.eval()
+    correct = 0
+    with torch.no_grad():
+        for input, target in test_loader:
+            output, _ = model(input)
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_acc = correct / len(test_loader.dataset)
+    print('[Test set] Epoch: {:d}, Accuracy: {:.2f}%\n'.format(
+        epoch+1, 100. * test_acc))
+
+    return test_acc
